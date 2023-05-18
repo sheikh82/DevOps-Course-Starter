323a73d099ead47210075bfa4a36433acb95f6a4
sheikh siddiquee <ssiddiquee@hotmail.co.uk>
2023-05-18 11:03:00 +0100

    Add Functionality to add todo items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from todo_app.data.session_items import get_items
+from flask import Flask, redirect, render_template, request
+from todo_app.data.session_items import add_item, get_items
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
@@ -11,6 +11,11 @@ def index():
     items = get_items()
     return render_template('index.html', items = items)
 
+@app.route('/todo', methods = ["POST"] )
+def create_todo():
+    title = request.form.get('todo-title')
+    add_item(title)
+    return redirect("/")
 
 if __name__ == '__main__':
     app.run()
